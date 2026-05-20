0b0ecd223a027e21584c1be37a259a423a4871d1
pedroxhenrique <165857001+pedroxhenrique@users.noreply.github.com>
2026-05-19 22:00:31 -0300

    Substitui synthetic por findViewById

--- app/src/main/java/com/example/imc/MainActivity.kt
+++ app/src/main/java/com/example/imc/MainActivity.kt
@@ -6,17 +6,15 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import android.view.View
 import android.view.accessibility.AccessibilityEvent
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputLayout
-import kotlinx.android.synthetic.main.activity_main.*
 import java.text.MessageFormat
 
 class MainActivity : AppCompatActivity() {
 
-    // Declaração de variáveis para acessar os componentes
     private lateinit var editNome: EditText
     private lateinit var editPeso: EditText
     private lateinit var editAltura: EditText
@@ -29,7 +27,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Inicialização das variáveis com findViewById
         editNome = findViewById(R.id.editNome)
         editPeso = findViewById(R.id.editPeso)
         editAltura = findViewById(R.id.editAltura)
@@ -50,7 +47,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        // Adicionando TextWatchers
         editNome.addTextChangedListener(clearErrorMessage(layoutEditNome))
         editPeso.addTextChangedListener(clearErrorMessage(layoutEditPeso))
         editAltura.addTextChangedListener(clearErrorMessage(layoutEditAltura))
@@ -61,35 +57,34 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-    private fun exibirMensagemErro(editText: EditText, textViewMessage: TextInputLayout, mensagem: String ) {
+    private fun exibirMensagemErro(editText: EditText, textViewMessage: TextInputLayout, mensagem: String) {
         textViewMessage.error = mensagem
         editText.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_FOCUSED)
-        editText.requestFocus();
+        editText.requestFocus()
     }
 
-    fun exibirMensagemErroNome(){
+    fun exibirMensagemErroNome() {
         val mensagem = formatarMensagem("Campo")
         exibirMensagemErro(editNome, layoutEditNome, mensagem)
     }
 
-    fun exibirMensagemErroPeso(){
+    fun exibirMensagemErroPeso() {
         val mensagem = formatarMensagem("Campo")
         exibirMensagemErro(editPeso, layoutEditPeso, mensagem)
     }
 
-    fun exibirMensagemErroAltura(){
+    fun exibirMensagemErroAltura() {
         val mensagem = formatarMensagem("Campo")
         exibirMensagemErro(editAltura, layoutEditAltura, mensagem)
     }
 
     private fun validarCamposBasicos(): Boolean {
-        Log.e("ERRO", editNome.toString()+"dsbhfkjsdhf")
+        Log.e("ERRO", editNome.toString() + "dsbhfkjsdhf")
         clearErrorMessage(layoutEditNome)
-        val tam =editNome.text.toString().length
+        val tam = editNome.text.toString().length
         if (isEmpty(editNome.text.toString()) || tam < 3) {
-                exibirMensagemErroNome()
-                return false
+            exibirMensagemErroNome()
+            return false
         }
         clearErrorMessage(layoutEditPeso)
 
@@ -108,16 +103,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun isEmpty(valor: String) = valor == ""
 
-    private fun formatarMensagem(campo:String) : String{
+    private fun formatarMensagem(campo: String): String {
         val message = "Informe o campo"
-        return  MessageFormat.format(message, campo)
+        return MessageFormat.format(message, campo)
     }
 
-    private fun clearErrorMessage(text:TextInputLayout): TextWatcher {
+    private fun clearErrorMessage(text: TextInputLayout): TextWatcher {
         return object : TextWatcher {
             override fun afterTextChanged(s: Editable) {}
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int ) { }
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int ) {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
                 text.error = ""
             }
         }
